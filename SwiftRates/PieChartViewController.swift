0651beba7c671ee38e051af1b1169a1e52ea8529
Seab <honeyboyseab@gmail.com>
2017-02-07 22:22:13 -0500

    implemented delegate and datasource methods to display the pie chart

--- SwiftRates/PieChartViewController.swift
+++ SwiftRates/PieChartViewController.swift
@@ -38,24 +38,112 @@ class PieChartViewController: UIViewController {
     symbols.insert(base, at: 0)
   }
   
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    initPlot()
+  }
+  
+  func initPlot() {
+    configureHostView()
+    configureGraph()
+    configureChart()
+    configureLegend()
+  }
+  
+  func configureHostView() {
+    hostView.allowPinchScaling = false
+  }
+  
+  func configureGraph() {
+    // create an configure the graph
+    let graph = CPTXYGraph(frame: hostView.bounds)
+    hostView.hostedGraph = graph
+    graph.paddingLeft = 0.0
+    graph.paddingTop = 0.0
+    graph.paddingRight = 0.0
+    graph.paddingBottom = 0.0
+    graph.axisSet = nil
+    
+    // create text styles
+    let textStyle: CPTMutableTextStyle = CPTMutableTextStyle()
+    textStyle.color = CPTColor.black()
+    textStyle.fontName = "HelveticaNeue-Bold"
+    textStyle.fontSize = 16.0
+    textStyle.textAlignment = .center
+    
+    // set graph title and text style
+    graph.title = "\(base.name) exchange rates\n\(rate.date)"
+    graph.titleTextStyle = textStyle
+    graph.titlePlotAreaFrameAnchor = CPTRectAnchor.top
+  }
+  
+  func configureChart() {
+    // get a reference to the graph
+    
+    let graph = hostView.hostedGraph!
+    
+    let pieChart = CPTPieChart()
+    pieChart.delegate = self
+    pieChart.dataSource = self
+    pieChart.pieRadius = (min(hostView.bounds.size.width, hostView.bounds.size.height) * 0.7) / 2
+    pieChart.identifier = NSString(string: graph.title!)
+    pieChart.startAngle = CGFloat(M_PI_4)
+    pieChart.sliceDirection = .clockwise
+    pieChart.labelOffset = 0.6 * pieChart.pieRadius
+    
+    // configure border style
+    let borderStyle = CPTMutableLineStyle()
+    borderStyle.lineColor = CPTColor.white()
+    borderStyle.lineWidth = 2.0
+    pieChart.borderLineStyle = borderStyle
+    
+    // configure text style
+    let textStyle = CPTMutableTextStyle()
+    textStyle.color = CPTColor.white()
+    textStyle.textAlignment = .center
+    pieChart.labelTextStyle = textStyle
+    
+    // add chart to the graph
+    graph.add(pieChart)
+  }
+  
+  func configureLegend() {
+    
+  }
+  
 }
 
 extension PieChartViewController: CPTPieChartDataSource, CPTPieChartDelegate {
   
   func numberOfRecords(for plot: CPTPlot) -> UInt {
-    return 0
+    return UInt(symbols.count)
   }
   
   func number(for plot: CPTPlot, field fieldEnum: UInt, record idx: UInt) -> Any? {
-    return 0
+    let symbol = symbols[Int(idx)]
+    let currencyRate = rate.rates[symbol.name]!.floatValue
+    return 1.0 / currencyRate
   }
   
   func dataLabel(for plot: CPTPlot, record idx: UInt) -> CPTLayer? {
-    return nil
+    let value = rate.rates[symbols[Int(idx)].name]!.floatValue
+    let layer = CPTTextLayer(text: String(format: "\(symbols[Int(idx)].name)\n%.2f", value))
+    layer.textStyle = plot.labelTextStyle
+    return layer
   }
   
   func sliceFill(for pieChart: CPTPieChart, record idx: UInt) -> CPTFill? {
-    return nil
+    switch idx {
+    case 0:
+      return CPTFill(color: CPTColor(componentRed: 0.92, green: 0.28, blue: 0.25, alpha: 1.00))
+    case 1:
+      return CPTFill(color: CPTColor(componentRed: 0.06, green: 0.80, blue: 0.48, alpha: 1.00))
+    case 2:
+      return CPTFill(color: CPTColor(componentRed: 0.22, green: 0.33, blue: 0.49, alpha: 1.00))
+    default:
+      return nil
+
+    }
   }
   
   func legendTitle(for pieChart: CPTPieChart, record idx: UInt) -> String? {
